409b56a29c3213bcae01854a6edf099582046816
camouflageme11 <72007567+camouflageme11@users.noreply.github.com>
2022-05-25 23:20:25 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,42 @@
+console.log('hello');
+document.getElementById('someText').innerHTML = 'hello';
+
+let name = {first: 'madhu' , last: 'shalini'} // object in js
+let fruits = ['apples','banana', 'oranges'] // array
+let fruit = 'banana';
+console.log(fruit.length);
+console.log(fruit.indexOf('n'));
+console.log(fruit.slice(2,6));
+console.log(fruit.split(',')); // split by comma
+console.log(fruit.split('')); ///split by character
+//alert('yoo'); inline comment
+//variable
+/*var v1 = 'madhu';
+var v2 = 45;
+console.log(v1);
+console.log(v2);
+
+document.getElementById('someText').innerHTML = 'printing using inner html in JS';
+//var age = prompt('what is your age?');
+//document.getElementById('someText').innerHTML = 'age is : '+ age;
+function fun(v3){
+    console.log('this is' + v3);
+}
+
+fun('mee');
+
+function add(a, b)
+{
+    console.log(a+b);
+}
+add(1,2);*/
+/*var num = 0;
+while(num < 100)
+{
+    console.log(num);
+    num ++;
+}
+for(let num =0; num <= 100; num++)
+{
+    console.log(num);
+}*/
